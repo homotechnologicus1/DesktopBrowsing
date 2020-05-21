150eb905c5c0a0d3911308840d011e2abdbfb897
joe <homo.technologicus1@gmail.com>
2020-05-22 02:19:09 +0900

    Initial Commit

---
+++ DesktopBrowsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesktopBrowsing
+//
+//  Created by joe_mac on 05/22/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
